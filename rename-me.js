93959234a561bbfb010171aa373ed87cd419f6a6
Carmen Johnson <blackpalmmobileapplications@gmail.com>
2012-05-09 13:49:46 -0700

    Update rename-me.js

--- rename-me.js
+++ rename-me.js
@@ -8,4 +8,9 @@ var companyA = “AppStars”;
 var timeA = 3
 var companyB = “CodeBang”;
 var timeB = 4
+expA =[“graphics”, “designing”, “designers”, “animation”];
+expB = [“coding”, “programming”, “java”]
+var bestDesigner = expA = expA.length;
+var pickCompany = function (whatCompany) {
+       var hireCompany = hireCompanies [whatCompany],
 
