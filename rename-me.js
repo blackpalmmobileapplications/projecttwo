d057ce9260e4202f1035fd2618594a2e1dd25c57
Carmen Johnson <blackpalmmobileapplications@gmail.com>
2012-05-09 13:49:01 -0700

    Update rename-me.js

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,11 @@
 alert("JavaScript works!");
+
+var des = “designer team”;
+var prog = “programming team”;
+var ht = “hire”;
+var hireCompanies = [“AppStart”, “CodeBang”]
+var companyA = “AppStars”;
+var timeA = 3
+var companyB = “CodeBang”;
+var timeB = 4
+
